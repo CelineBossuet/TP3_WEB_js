19fb5514cafb7723ba332312432ff0b5177d9dcb
bossuetc <bossuet.celine@grenoble-inp.org>
2022-02-17 14:22:21 +0100

    modifs

--- Exercice-42.js
+++ Exercice-42.js
@@ -1,17 +1,23 @@
 window.onload=function(){
     function liste(i){
         var list = getDefinition(i)
+        var contents =document.getElementById("contents")
+        while(contents.firstChild!=null){
+            contents.removeChild(contents.firstChild)
+        }
         affiche(list)
 
     }
 
     function affiche(list){
+        console.log("Je rentre")
         var elem  = document.getElementById("contents")
         var dl = document.createElement("dl")
 
         // title
         var dt = document.createElement("dt")
         var text = document.createTextNode(list["title"])
+        console.log("Mon titre : "+list["title"])
         dt.appendChild(text)
         dl.append(dt)
         
@@ -21,20 +27,20 @@ window.onload=function(){
         
         for (i = 0; i < items.length; i++){
             var li = document.createElement("li")
-            if (typeof items[i] == "string"){
-                console.log("int")
-                var item = document.createTextNode(items[i]+" ")
+            if (typeof items[i] == "string" ){
+                console.log("j'ajoute "+items[i])
+                var item = document.createTextNode(items[i])
                 li.appendChild(item)
             }
             else{
-                console.log("autre")
+                console.log(items[i])
                 affiche(items[i])
             }
             dd.append(li)
         }
         dl.append(dd)
         elem.appendChild(dl)
-        console.log(elem)
+        //console.log(elem)
     }
 
 
